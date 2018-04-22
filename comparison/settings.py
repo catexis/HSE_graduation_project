dd92ab22380f099f568eb57447e7dc6f7df180e1
catexis <anderson.perm@gmail.com>
2018-04-22 21:11:20 +0500

    Static added
    Добавлена первая страница сайта и подключена статитка

--- comparison/settings.py
+++ comparison/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f(7zggia#s^j*j!z-+xwd@j^)@18^fz4t$je6to1(kk&e=dyq_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vergleich.apps.VergleichConfig'
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Yekaterinburg'
 
 USE_I18N = True
 
